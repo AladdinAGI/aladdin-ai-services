0afd6c9191c8072818a566d284b7657ef79f69a8
Hrishabh Ayush <hrishabh.ay@gmail.com>
2025-02-13 13:11:21 -0500

    lambda.ts changed

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,5 +2,5 @@
 import serverless from 'serverless-http';
 import app from './main';
 
-// 将 Koa 应用包装为 Lambda 处理函数
+// Wrap Koa app in Lambda handler function
 export const handler = serverless(app);
